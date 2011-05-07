4ec27237423a6e2aa8065e4985b10c168e74886b
Ian Luckraft <ianckc@mba.ac.uk>
2011-05-07 15:12:14 +0100

    First commit of the library. Refer to README onhow to use in a CodeIgniter based application

---
+++ system/language/english/instagram_api_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Instagram
+|--------------------------------------------------------------------------
+|
+| Instagram client details
+|
+*/
+
+$lang['client_name']	= 'REPLACE WITH YOUR INSTAGRAM CLIENT NAME';				// Instagram client name
+$lang['client_id']		= 'REPLACE WITH YOUR INSTAGRAM CLIENT ID';					// Instagram client id
+$lang['client_secret']	= 'REPLACE WITH YOUR INSTAGRAM CLIENT SECRET';				// Instagram client secret
+$lang['callback_url']	= 'REPLACE WITH YOUR INSTAGRAM CALLBACK URL';				// Your callback url
+$lang['website']		= 'REPLACE WITH YOUR WEBSITE';								// Your website
+$lang['description']	= 'REPLACE WITH YOUR INSTAGRAM APPLICATION DESCRIPTION';	// Your application description
