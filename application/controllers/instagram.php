e32841c3a1cfd8dc4943e7ddabb396e561cc256d
James Chevalier <jchevalier@gmail.com>
2011-06-29 20:45:12 -0400

    no longer requires separate configuration file
    * Completed migration away from the requirement for a separate configuration file
    * Modified some functions to accept passed variables (part of migration away from separate configuration file)
    * Implemented new functions in provided Controller
    * Added a bit more information to the README

--- application/controllers/instagram.php
+++ application/controllers/instagram.php
@@ -9,8 +9,11 @@ class Instagram extends CI_Controller {
 	function index()
 	{
 		$this->load->library('instagram_api');
+		$ig_client_id = $this->config->item('instagram_client_id');
+		$ig_redirect_uri = $this->config->item('instagram_redirect_uri');
+
 		echo "<p>This library uses the Instagram API but is not endorsed or certified by Instagram.</p>";
-		echo "<a href=\"https://api.instagram.com/oauth/authorize/?client_id=".$this->config->item('instagram_client_id')."&redirect_uri=".$this->config->item('instagram_redirect_uri')."&response_type=code\">Authorize Instagram</a>";
+		echo "<a href=\"".$this->instagram_api->instagramLogin($ig_client_id, $ig_redirect_uri)."\">Authorize Instagram</a>";
 	}
 	
 /*
@@ -20,8 +23,12 @@ class Instagram extends CI_Controller {
 	function instagramredirect()
 	{
 		$this->load->library('instagram_api');		
-		$igObj = new Instagram_api($this->config->item('instagram_client_id'), $this->config->item('instagram_client_secret'));
-		$token = $igObj->authorize($_GET['code']);
+		$ig_client_id = $this->config->item('instagram_client_id');
+		$ig_client_secret = $this->config->item('instagram_client_secret');
+		$ig_redirect_uri = $this->config->item('instagram_redirect_uri');
+
+		$igObj = new Instagram_api($ig_client_id, $ig_client_secret);
+		$token = $igObj->authorize($ig_client_id, $ig_client_secret, $ig_redirect_uri, $_GET['code']);
 		echo "Copy the next line of text into the value for \"instagram_access_token\" in the application/config/config.php file:<br>".$token->access_token;
 	}
 
